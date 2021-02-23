15f2dc748d2c4ab09f0a6f0aa5f0bad46c1faf76
Murray Smith <murray@mismith.info>
2021-02-22 21:44:12 -0700

    add reset button

--- src/views/Recorder.vue
+++ src/views/Recorder.vue
@@ -6,6 +6,7 @@
         <label>
           Plant(s)
           <button type="button" @click="isAdding = !isAdding">Add Plant</button>
+          <button type="reset" @click="handleReset">Reset</button>
         </label>
         <PlantTreeView multiple v-model="plantIds" />
       </fieldset>
@@ -82,6 +83,61 @@ export default defineComponent({
     const weight = ref<number>()
     const weightUnit = ref<string>('g')
 
+    function handleReset() {
+      plantIds.value = []
+      newBedIds.value = []
+      weight.value = undefined
+      weightUnit.value = 'g'
+      at.value = undefined
+      note.value = undefined
+    }
+    async function handleSubmit() {
+      await Promise.all(plantIds.value.map(async (plantId) => {
+        let payload: Record<string, any> | null = null;
+        switch (eventId.value) {
+          case 'transplant': {
+            const newBedId = newBedIds.value?.[0]
+            if (!newBedId) return // @TODO
+
+            const bedIdRef = database.ref(`/users/mismith/plants/${plantId}/bedId`)
+            const oldBedId = (await bedIdRef.once('value')).val()
+            payload = {
+              oldBedId,
+              newBedId,
+            }
+            await bedIdRef.set(newBedId)
+            break
+          }
+          case 'harvest': {
+            if (weight) {
+              payload = {
+                weight: {
+                  value: weight.value,
+                  unit: weightUnit.value,
+                }
+              }
+            }
+            break
+          }
+          case 'cull': {
+            const bedIdRef = database.ref(`/users/mismith/plants/${plantId}/bedId`)
+            await bedIdRef.remove()
+            break
+          }
+        }
+
+        await database.ref(`/users/mismith/plants/${plantId}/entries`).push({
+          eventId: eventId.value,
+          at: at.value ? new Date(at.value).valueOf() : ServerValue.TIMESTAMP,
+          payload,
+          note: note.value || null,
+          createdAt: ServerValue.TIMESTAMP,
+        } as Omit<Entry, 'id'>)
+      }))
+      
+      handleReset()
+    }
+
     return {
       isAdding,
       plantIds,
@@ -94,56 +150,8 @@ export default defineComponent({
       weight,
       weightUnit,
 
-      async handleSubmit() {
-        await Promise.all(plantIds.value.map(async (plantId) => {
-          let payload: Record<string, any> | null = null;
-          switch (eventId.value) {
-            case 'transplant': {
-              const newBedId = newBedIds.value?.[0]
-              if (!newBedId) return // @TODO
-
-              const bedIdRef = database.ref(`/users/mismith/plants/${plantId}/bedId`)
-              const oldBedId = (await bedIdRef.once('value')).val()
-              payload = {
-                oldBedId,
-                newBedId,
-              }
-              await bedIdRef.set(newBedId)
-              break
-            }
-            case 'harvest': {
-              if (weight) {
-                payload = {
-                  weight: {
-                    value: weight.value,
-                    unit: weightUnit.value,
-                  }
-                }
-              }
-              break
-            }
-            case 'cull': {
-              const bedIdRef = database.ref(`/users/mismith/plants/${plantId}/bedId`)
-              await bedIdRef.remove()
-              break
-            }
-          }
-
-          await database.ref(`/users/mismith/plants/${plantId}/entries`).push({
-            eventId: eventId.value,
-            at: at.value ? new Date(at.value).valueOf() : ServerValue.TIMESTAMP,
-            payload,
-            note: note.value || null,
-            createdAt: ServerValue.TIMESTAMP,
-          } as Omit<Entry, 'id' | 'createdAt'>)
-        }))
-        plantIds.value.splice(0, plantIds.value.length)
-        newBedIds.value.splice(0, newBedIds.value.length)
-        weight.value = undefined
-        weightUnit.value = 'g'
-        at.value = undefined
-        note.value = undefined
-      },
+      handleReset,
+      handleSubmit,
     }
   },
 })
